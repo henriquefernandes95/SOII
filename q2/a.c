ca455857c53a4597944ffc4920a46e56c8877d95
Giovanni Oliveira <giovanni.oliveira@neopath.com.br>
2019-09-18 10:43:08 -0300

    finishing question 2

--- q2/a.c
+++ q2/a.c
@@ -1,15 +1,23 @@
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
 
-int walk_dir(const char *path, void (*func)(const char *))
+#define PATH "/home/goliveira/test"
+#define VALID_OPTS "rdlbc"
+
+
+int walk_dir(const char *path, char type)
 {
     DIR *dirp;
     struct dirent *dp;
     char *p, *full_path;
     int len;
+    char full_name[NAME_MAX+1];
+    int cnt = 0;
     /* abre o diretório */
     if ((dirp = opendir(path)) == NULL)
         return (-1);
@@ -29,15 +37,40 @@ int walk_dir(const char *path, void (*func)(const char *))
         /* ignora as entradas “.” e “..” */
         if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
             continue;
+
         strcpy(p, dp->d_name);
-        /* “full_path” armazena o caminho */
-        (*func)(full_path);
+
+        struct stat filestat;
+        stat(full_path,&filestat);
+
+        if(type=='r' && S_ISREG(filestat.st_mode) ||
+           type=='d' && S_ISDIR(filestat.st_mode) ||
+           type=='l' && S_ISLNK(filestat.st_mode) ||
+           type=='b' && S_ISBLK(filestat.st_mode) ||
+           type=='c' && S_ISCHR(filestat.st_mode))
+           cnt++;
+        
+        ///* “full_path” armazena o caminho */
+        //(*func)(full_path);
     }
     free(full_path);
     closedir(dirp);
-    return (0);
+    return (cnt);
 } /* end walk_dir */
 
-int main(){
+int main(int argc, char **argv){
+    
+    int ret;
+    int opt;
+    char *path;
+
+    opt = getopt(argc,argv,VALID_OPTS);
+    if(opt == -1){
+        ret = walk_dir(argv[1],'r');
+    }else{
+        ret = walk_dir(argv[2],opt);
+    }
+
+    printf("%d\n",ret);
     return 0;
 }
