e88492458a85700bb065c9d7c6ff15de6ce1da3e
Henrique <hfr@gmx.co.uk>
2019-10-05 17:20:43 -0300

    pipe

--- TrabIII/przombies.c
+++ TrabIII/przombies.c
@@ -2,31 +2,46 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-int child(int sec){
+
+int main(int argc, char *argv[]){
+    int child, cstatus, filed,fsize;
+    char *buf;
+    int fd[2];
     char dir[256];
-    char *ps="/bin/ps";
-    char *log="> log.txt";
-    printf("passou");
+
     while(true){
         getcwd(dir,sizeof(dir));
+        filed=open(strcat(dir,"/log.txt"),O_RDWR, S_IRUSR |S_IXOTH);
         //puts(dir);
-        execl(ps,log);
-        printf("%d",sec);
-        sleep(sec*1000);
+        //child=fork();
+        pipe(fd);
+        //child=popen();
+        printf("PROCESSO DE ID %d",child);
+        if(!child){
+            printf("passou");
+            close(fd[0]);
+            execl("/bin/ps","/bin/ps","-aux",(char*)0);
+            
+        }
+        else{
+            close(fd[1]);
+            fsize=lseek(fd[0],0,SEEK_END);
+            read(fd[0],buf,fsize);
+            write(filed,read,sizeof(fd[0]));
+            wait(&cstatus);
+            //printf("%d",sec);
+            sleep(atoi(argv[1]));
+            kill(child,SIGTERM);
+            waitpid(child,&cstatus,0);
+        }
         
     }
     
-    return 0;
-}
-
-int main(int argc, char *argv[]){
-    
-
-    if(fork()==0){
-        printf("%d",atoi(argv[1]));
-        child(atoi(argv[1]));
-    }
 
 
     
