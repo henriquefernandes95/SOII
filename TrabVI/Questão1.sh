46d6509caea1fad68b8fa9734d7ae2b3366c472d
Henrique <hfr@gmx.co.uk>
2019-11-12 19:35:16 -0300

    Questao1 terminada

--- TrabVI/Questão1.sh
+++ TrabVI/Questão1.sh
@@ -4,19 +4,19 @@ read x
 if [ $x = 's' ] 
     then
         echo "O horário é:"
-        date +%X
+        date +%X #exibe somente o horario d)
 fi
 echo Deseja saber o uso \do disco? s ou n
 read x
 if [ $x = 's' ] 
     then
         echo "O uso do disco é:"
-        du /* -hsxc 2>/dev/null | grep -w [0-9]*[GMK]
+        du /* -hsxc 2>/dev/null | grep -w [0-9]*[GMK] #retorna o uso dos dados com aparencia amigavel e desviando os possiveis erros para o null, obtendo somente os dados relevantes
 fi
 echo Deseja os usuários? s ou n
 read x
 if [ $x = 's' ]  
     then
         echo "Os usuários logados são:"
-        who
+        who #lista os usuários logados
 fi
