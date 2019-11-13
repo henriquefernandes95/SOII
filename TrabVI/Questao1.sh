23d47cd672204fc9db987f03536d0b6babf6fba6
Henrique <hfr@gmx.co.uk>
2019-11-13 00:33:38 -0300

    Fix

---
+++ TrabVI/Questao1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo Deseja o horário? s ou n
+read x
+if [ $x = 's' ] 
+    then
+        echo "O horário é:"
+        date +%X #exibe somente o horario d)
+fi
+echo Deseja saber o uso \do disco? s ou n
+read x
+if [ $x = 's' ] 
+    then
+        echo "O uso do disco é:"
+        du /* -hsxc 2>/dev/null | grep -w [0-9]*[GMK] #retorna o uso dos dados com aparencia amigavel e desviando os possiveis erros para o null, obtendo somente os dados relevantes
+fi
+echo Deseja os usuários? s ou n
+read x
+if [ $x = 's' ]  
+    then
+        echo "Os usuários logados são:"
+        who #lista os usuários logados
+fi
